602b2c0db3b29936b783361603f62f7eb6a892f8
shimonah <provodnik357@gmail.com>
2019-07-16 16:25:17 +0300

    Update add-elasticsearch.sh

--- elastic-search/add-elasticsearch.sh
+++ elastic-search/add-elasticsearch.sh
@@ -1,6 +1,7 @@
 # add container for specific elasticsearch version
-sudo docker run -dti --restart always --net net --name elasticsearch2 	-p9200:9200 		-v /var/www/elasticsearch2:/usr/share/elasticsearch/data 		elasticsearch:2.4
-sudo docker run -dti --restart always --net net --name elasticsearch5 	-p9201:9200 		-v /var/www/elasticsearch5:/usr/share/elasticsearch/data 		elasticsearch:5
+sudo docker run -dti --restart always --net net --name elasticsearch2 -p9200:9200 -v /var/www/elasticsearch2:/usr/share/elasticsearch/data elasticsearch:2.4
+sudo docker run -dti --restart always --net net --name elasticsearch5 -p9201:9200 -v /var/www/elasticsearch5:/usr/share/elasticsearch/data elasticsearch:5
+
 # intall plugin for elasticsearch for correct working of elasticsearch
 sudo docker exec -ti elasticsearch5 bash -c "bin/elasticsearch-plugin install analysis-icu"
 sudo docker exec -ti elasticsearch5 bash -c "bin/elasticsearch-plugin install analysis-phonetic"
